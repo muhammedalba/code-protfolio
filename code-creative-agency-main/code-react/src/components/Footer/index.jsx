1848cca2445cc151e6387d51430c94df9cece91c
muhammedalbahle <muhammedalbahle@gmail.com>
2024-09-30 14:39:41 +0300

    logo

---
+++ code-creative-agency-main/code-react/src/components/Footer/index.jsx
@@ -0,0 +1,89 @@
+import React from 'react';
+import Div from '../Div';
+import ContactInfoWidget from '../Widget/ContactInfoWidget';
+import MenuWidget from '../Widget/MenuWidget';
+import Newsletter from '../Widget/Newsletter';
+import SocialWidget from '../Widget/SocialWidget';
+import TextWidget from '../Widget/TextWidget';
+import './footer.scss';
+const copyrightLinks = [
+  {
+    title: 'Terms of Use',
+    href: '/',
+  },
+  {
+    title: 'Privacy Policy',
+    href: '/',
+  },
+];
+
+const serviceMenu = [
+  {
+    title: 'UI/UX design',
+    href: '/service/service-details',
+  },
+  {
+    title: 'WP development',
+    href: '/service/service-details',
+  },
+  {
+    title: 'Digital marketing',
+    href: '/service/service-details',
+  },
+  {
+    title: 'React development',
+    href: '/service/service-details',
+  },
+];
+
+export default function Footer({ copyrightText, logoSrc, logoAlt, text }) {
+  return (
+    <footer className="cs-fooer">
+      <Div className="cs-fooer_main">
+        <Div className="container">
+          <Div className="row">
+            <Div className="col-lg-3 col-sm-6">
+              <Div className="cs-footer_item">
+                <TextWidget
+                  logoSrc="/images/code-logo.png"
+                  logoAlt="Logo"
+                  text="Welcome to codeprops .We are a company specialized in providing communication technology solutions for the requirements of companies in various industries."
+                />
+                <SocialWidget />
+              </Div>
+            </Div>
+            <Div className="col-lg-3 col-sm-6">
+              <Div className="cs-footer_item">
+                <MenuWidget menuItems={serviceMenu} menuHeading="Services" />
+              </Div>
+            </Div>
+            <Div className="col-lg-3 col-sm-6">
+              <Div className="cs-footer_item">
+                <ContactInfoWidget title="Contact Us" />
+              </Div>
+            </Div>
+            <Div className="col-lg-3 col-sm-6">
+              <Div className="cs-footer_item">
+                <Newsletter
+                  title="Subscribe"
+                  subtitle="At vero eos et accusamus et iusto odio as part dignissimos ducimus qui blandit."
+                  placeholder="example@gmail.com"
+                />
+              </Div>
+            </Div>
+          </Div>
+        </Div>
+      </Div>
+      <Div className="container">
+        <Div className="cs-bottom_footer">
+          <Div className="cs-bottom_footer_left">
+            <Div className="cs-copyright">Copyright © 2022 codeprops.</Div>
+          </Div>
+          {/* <Div className="cs-bottom_footer_right">
+            <MenuWidget menuItems={copyrightLinks} variant=" cs-style2" />
+          </Div> */}
+        </Div>
+      </Div>
+    </footer>
+  );
+}
